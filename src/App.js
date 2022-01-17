7114bab2c70b19b10d433e183bd93357c7412458
multios12 <5012052+multios12@users.noreply.github.com>
2022-01-17 21:48:53 +0900

    fix: count-upの実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Button, TextField, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+import axios from 'axios';
+import { Box } from '@mui/system';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: "5" }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              count-up
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box  sx={{ m: 2 }}>
+          <TextField id="standard-basic" label="Standard" value={this.state.value} onChange={this.changeValue} />
+          <Button variant="contained" color="primary" onClick={this.send}>送信</Button>
+        </Box>
+      </div>
+    );
+  }
+  send = () => {
+    const url = "/api/counter?value=" + this.state.value;
+    axios.get(url ).then((r) => {
+      const s = { value: r.data.value };
+      this.setState(s);
+    })
+  }
+  changeValue(newValue) {
+    this.setState({ value: newValue });
+  }
 }
 
 export default App;
---
+++ srv/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pino = require('express-pino-logger')();
+
+const app = express(); app.use(express.urlencoded({ extended: false }));
+app.use(pino);
+
+app.get('/api/counter', (req, res) => {
+    let value = Number(req.query.value);
+    value = value + 1;
+    res.setHeader('Content-Type', 'application/json');
+
+    res.send(JSON.stringify({ value: value }));
+});
+
+app.get('/api/test', (req, res) => {
+    const name = req.query.name || 'World';
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ message: `Hello ${name}!` }));
+});
+
+app.listen(3001, () => console.log('Express server is running on localhost:3001'));
